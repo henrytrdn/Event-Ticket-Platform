642e206c97d77aacd71dac7484e1fb63c1879ecc
Henry Tran <htra0037@uni.sydney.edu.au>
2026-02-27 16:59:08 +1100

    feat: Implement EventServiceImpl and EventRepository to save an Event

--- src/main/java/com/example/tickets/exceptions/EventTicketException.java
+++ src/main/java/com/example/tickets/exceptions/EventTicketException.java
@@ -1,4 +1,22 @@
 package com.example.tickets.exceptions;
 
-public class EventTicketException {
+public class EventTicketException extends RuntimeException {
+    public EventTicketException() {
+    }
+
+    public EventTicketException(String message) {
+        super(message);
+    }
+
+    public EventTicketException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EventTicketException(Throwable cause) {
+        super(cause);
+    }
+
+    public EventTicketException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
--- src/main/java/com/example/tickets/services/impl/EventServiceImpl.java
+++ src/main/java/com/example/tickets/services/impl/EventServiceImpl.java
@@ -1,4 +1,53 @@
 package com.example.tickets.services.impl;
 
-public class EventServiceImpl {
+import com.example.tickets.domain.CreateEventRequest;
+import com.example.tickets.domain.entities.Event;
+import com.example.tickets.domain.entities.TicketType;
+import com.example.tickets.domain.entities.User;
+import com.example.tickets.exceptions.UserNotFoundException;
+import com.example.tickets.repositories.EventRepository;
+import com.example.tickets.repositories.UserRepository;
+import com.example.tickets.services.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor // Generates a constructor based on instance variables that are final
+public class EventServiceImpl implements EventService {
+
+    private final UserRepository userRepository;
+    private final EventRepository eventRepository;
+
+    @Override
+    public Event createEvent(UUID organizerId, CreateEventRequest event) {
+        User organizer = userRepository.findById(organizerId).orElseThrow(() -> new UserNotFoundException(
+                String.format("User with ID '%s' not found", organizerId))
+        );
+
+        // Convert list of TicketType DTOs to list of TicketType entities
+        List<TicketType> ticketTypesToCreate = event.getTicketTypes().stream().map(ticketType -> {
+            TicketType ticketTypeToCreate = new TicketType();
+            ticketTypeToCreate.setName(ticketType.getName());
+            ticketTypeToCreate.setPrice(ticketType.getPrice());
+            ticketTypeToCreate.setDescription(ticketType.getDescription());
+            ticketTypeToCreate.setTotalAvailable(ticketType.getTotalAvailable());
+            return ticketTypeToCreate;
+        }).toList();
+
+        Event eventToCreate = new Event();
+        eventToCreate.setName(event.getName());
+        eventToCreate.setStart(event.getStart());
+        eventToCreate.setEnd(event.getEnd());
+        eventToCreate.setVenue(event.getVenue());
+        eventToCreate.setSalesStart(event.getSalesStart());
+        eventToCreate.setSalesEnd(event.getSalesEnd());
+        eventToCreate.setStatus(event.getStatus());
+        eventToCreate.setOrganizer(organizer);
+        eventToCreate.setTicketTypes(ticketTypesToCreate);
+
+        return eventRepository.save(eventToCreate);
+    }
 }
