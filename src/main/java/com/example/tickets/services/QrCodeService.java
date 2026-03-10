77fc67cc59f2968414ea1e07ccbaccd766cbda65
Henry Tran <htra0037@uni.sydney.edu.au>
2026-03-10 23:42:04 +1100

    feat: Implement Get QR Code in service layer

--- src/main/java/com/example/tickets/services/QrCodeService.java
+++ src/main/java/com/example/tickets/services/QrCodeService.java
@@ -3,7 +3,11 @@ package com.example.tickets.services;
 import com.example.tickets.domain.entities.QrCode;
 import com.example.tickets.domain.entities.Ticket;
 
+import java.util.UUID;
+
 public interface QrCodeService {
 
     QrCode generateQrCode(Ticket ticket);
+
+    byte[] getQrCodeImageForUserAndTicket(UUID userId, UUID ticketId);
 }
