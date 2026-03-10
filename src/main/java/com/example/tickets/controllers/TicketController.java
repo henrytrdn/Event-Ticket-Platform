e6d0988f7b85c46475d1f0dd3c3ba25a4549d490
Henry Tran <htra0037@uni.sydney.edu.au>
2026-03-10 23:50:41 +1100

    feat: Implement Ticket QR code endpoint

--- src/main/java/com/example/tickets/controllers/TicketController.java
+++ src/main/java/com/example/tickets/controllers/TicketController.java
@@ -3,11 +3,14 @@ package com.example.tickets.controllers;
 import com.example.tickets.domain.dtos.GetTicketResponseDto;
 import com.example.tickets.domain.dtos.ListTicketResponseDto;
 import com.example.tickets.mappers.TicketMapper;
+import com.example.tickets.services.QrCodeService;
 import com.example.tickets.services.TicketService;
 import com.example.tickets.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -25,6 +28,7 @@ public class TicketController {
 
     private final TicketService ticketService;
     private final TicketMapper ticketMapper;
+    private final QrCodeService qrCodeService;
 
     @GetMapping
     public Page<ListTicketResponseDto> listTickets(
@@ -48,5 +52,32 @@ public class TicketController {
                 .map(dto -> ResponseEntity.ok(dto))
                 .orElse(ResponseEntity.notFound().build());
     }
+
+    @GetMapping(path = "/{ticketId}/qr-codes")
+    public ResponseEntity<byte[]> getTicketQrCode(
+            @AuthenticationPrincipal Jwt jwt,
+            @PathVariable UUID ticketId
+    ) {
+        byte[] qrCodeImage = qrCodeService.getQrCodeImageForUserAndTicket(
+                JwtUtil.parseUserId(jwt),
+                ticketId
+        );
+
+        HttpHeaders headers = new HttpHeaders();    // HTTP responses have a header (metadata) and a body (data)
+        headers.setContentType(MediaType.IMAGE_PNG);    // sets the HTTP header to display content type as PNG to display an image
+        headers.setContentLength(qrCodeImage.length); // Tells the client how many bytes long the body is
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(qrCodeImage);
+    }
+
+    // example http response
+    // Status: 200 OK
+    //Headers:
+    //   Content-Type: image/png
+    //   Content-Length: 5342
+    //Body:
+    //   [binary QR code image]
 
 }
