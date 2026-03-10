8e49630634a9656472b3e5d00422ea3484d4f2f9
Henry Tran <htra0037@uni.sydney.edu.au>
2026-03-10 22:52:32 +1100

    feat: Implement DTOs and mapper for List Ticket function

---
+++ src/main/java/com/example/tickets/mappers/TicketMapper.java
@@ -0,0 +1,16 @@
+package com.example.tickets.mappers;
+
+import com.example.tickets.domain.dtos.ListTicketResponseDto;
+import com.example.tickets.domain.dtos.ListTicketTicketTypeResponseDto;
+import com.example.tickets.domain.entities.Ticket;
+import com.example.tickets.domain.entities.TicketType;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface TicketMapper {
+
+    ListTicketTicketTypeResponseDto toListTicketTicketTypeResponseDto(TicketType ticketType);
+
+    ListTicketResponseDto toListTicketResponseDto(Ticket ticket);
+}
